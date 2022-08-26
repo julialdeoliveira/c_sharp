bd587252b246cbf9916cedb8d871770d4f00c25f
Júlia <julia_l_de-oliveira@estudante.sc.senai.br>
2022-08-26 13:47:42 -0300

    métodos get e post

---
+++ Warren/Data/Model/BaseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Model
+{
+    public abstract class BaseModel //O abstract faz ela só poder ser herdade e não instanciada
+    {
+        public int Id { get; set; } 
+            
+    }
+}
---
+++ Warren/WebApi/Controllers/AnimalController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Data.Model;
+using Data.Repository;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalController : ControllerBase
+    {
+        [HttpGet]
+        public Animal Get()
+        {
+            Animal a1 = new Animal {Id = 1,Especie = "cachorro", Nome = "mel" , Peso = 25.20f};
+            return a1;
+        }
+
+
+        [HttpPost]
+        public string Post(Animal model)
+        {
+            AnimalRepository repo = new AnimalRepository();
+            return repo.Create(model);
+            
+        }
+    }
+}
